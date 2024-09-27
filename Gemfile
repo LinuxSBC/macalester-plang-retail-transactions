6a6d124ed9d9d7f95d101eb70a0dd595315c9b86
LinuxSBC <benedict.p.simmons@gmail.com>
2024-09-27 15:47:48 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+ruby "3.3.5"
+
+gem "aasm"
+
+group :development do
+  gem "rake"
+end
+
+group :test do
+  gem "minitest"
+  gem "minitest-reporters"
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,54 @@
+# encoding: UTF-8
+
+# Require everything directly under lib/
+Dir[File.join(File.dirname(__FILE__), "../lib/*")].each do |lib_file|
+  require lib_file
+end
+
+require "minitest/autorun"
+require "minitest/reporters"
+
+class CustomReporter < Minitest::Reporters::DefaultReporter
+  def before_suite(suite)
+  end
+
+  def on_record(result)
+    super
+
+    test_path = result.klass.split("::")
+    test_path << result.name.gsub(/^test_\d+_/, '')
+    print " "
+    color = if result.skipped?
+      method(:yellow)
+    elsif result.failure
+      method(:red)
+    else
+      :itself
+    end
+    puts test_path.map(&color).join(blue(" · "))
+  end
+
+  def record_pass(record)
+    green("✔︎")
+  end
+
+  def record_skip(record)
+    yellow("S")
+  end
+
+  def record_failure(record)
+    red("✖︎")
+  end
+end
+
+unless ENV['RM_INFO']  # RubyMine has its own way of handling test results
+  Minitest::Reporters.use! CustomReporter.new
+end
+
+module Minitest::Assertions
+  def assert_invalid_transition
+    assert_raises AASM::InvalidTransition do
+      yield
+    end
+  end
+end
